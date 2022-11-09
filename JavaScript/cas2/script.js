a14bb722760323a3d56fbef282d76700ca90d63f
eris00 <erissutkovic00gmail.com>
2022-11-09 02:29:41 +0100

    m

---
+++ JavaScript/cas2/script.js
@@ -0,0 +1,38 @@
+
+
+// // array
+// let fruits = ["mango", "banana", "apple", "blueberry"]
+
+// // printing array
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+// // joining array
+// let s = fruits.join()
+// console.log("s " + s)
+
+// // kopiranje lokacije niza
+// numbers = [1, 2, 3];
+// numbersCopy = [...numbers];
+// console.log(numbersCopy);
+// numbersCopy.reverse()
+// console.log(numbersCopy);
+// console.log(numbers);
+
+// // sortiranje
+// arr = [2,1,4,5,6]
+// console.log(arr);
+
+// arr.sort(function sort(a,b) { return a-b })
+// console.log(arr)
+
+// // slice metoda
+// let a = [1,2,3,4,5];
+// console.log(a.slice(3));
+// console.log(a.slice(-3,-2));
+
+// // splice metoda
+// let a = [2,3,4,5,6]
+// console.log(a.splice(4,2));
+
